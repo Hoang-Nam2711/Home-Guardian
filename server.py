e7af37fe94d5f2e78d3995c5d128b1f23e925575
HoangNam2711 <hoang.nam271199@gmail.com>
2022-10-02 15:43:14 +0700

    Pi read sensor

---
+++ server.py
@@ -0,0 +1,28 @@
+import bluepy 
+import sys
+
+device = "8C:4B:14:15:9E:16"
+serviceUUID = "86f548d6-c323-479e-bbc7-defbcf190cd3"
+sensorUUID = "6d58fd7e-dfd1-48e3-a1aa-508a37674586"
+messageUUID = "9e3a3fe6-8887-4165-86ac-214c79b17cf2"
+
+def main():
+    connected = False
+    # Connect to device
+    while not (connected):
+        try:
+            server = bluepy.btle.Peripheral(device)
+            connected = True
+        except:
+            pass
+
+    #Get service and characteristic
+    service = server.getServiceByUUID(serviceUUID)
+    sensor = service.getCharacteristics(sensorUUID)[0]
+    message = service.getCharacteristics(messageUUID)[0]
+
+    #Get sensor value
+    human = int.from_bytes(sensor.read(), byteorder=sys.byteorder)
+    print(human)
+
+main()
