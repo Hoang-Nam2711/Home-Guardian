b6d885be31724c0417ef8d066092f5059a4393a1
HoangNam2711 <hoang.nam271199@gmail.com>
2022-10-08 22:39:00 +0700

    Rename video file

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
 import bluepy 
 import sys
+from picamera import PiCamera
+import os
+import datetime
+import time
 
 device = "8C:4B:14:15:9E:16"
 serviceUUID = "86f548d6-c323-479e-bbc7-defbcf190cd3"
 sensorUUID = "6d58fd7e-dfd1-48e3-a1aa-508a37674586"
 messageUUID = "9e3a3fe6-8887-4165-86ac-214c79b17cf2"
+
+camFolder = "./camVid"
+
+def camSave():
+    time = datetime.datetime.today()
+    fileName = "humanDetect-"+str(time.day)+str(time.month)+str(time.year)+"-"+str(time.hour)+str(time.minute)+str(time.second)
+    print(fileName)
+    return camFolder + "/" + fileName  +".h264"
+
+def camera():
+    camera=PiCamera()
+    camera.start_recording(camSave())
+    time.sleep(60)
+    camera.stop_recording()
 
 def main():
     connected = False
@@ -12,6 +30,7 @@ def main():
     while not (connected):
         try:
             server = bluepy.btle.Peripheral(device)
+            print("a")
             connected = True
         except:
             pass
@@ -22,7 +41,10 @@ def main():
     message = service.getCharacteristics(messageUUID)[0]
 
     #Get sensor value
+    # while(1):
     human = int.from_bytes(sensor.read(), byteorder=sys.byteorder)
-    
+    print(human)
+    if(human):
+        camera()
 
 main()
